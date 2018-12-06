fec1fddbcb3c8364445b26d545efd281a46aac4c
Vinay Swamy <swamyvinny@gmail.com>
2018-12-06 13:27:44 -0500

    attempting to paralellize

--- scripts/make_matrix.R
+++ scripts/make_matrix.R
@@ -1,16 +1,21 @@
 library(dplyr)
 library(GenomicRanges)
 library(gread)
-# gene=commandArgs(trailingOnly = T)[1]
+library(parallel)
+args=commandArgs(trailingOnly = T)
 # gene='OR4F5'
-targ_peaks <- 'data/IPSC_ATAC.common_peaks.blackListed.bed'
+#targ_peaks <- 'data/IPSC_ATAC.common_peaks.blackListed.bed'
+targ_peaks <- args[1]
 target_peaks=read.table(targ_peaks, stringsAsFactors = F,sep = '\t')
 colnames(target_peaks) <- c('p_chr','p_start','p_end','peak')
 # gread library lets you add introns into a gtf easily
 gtf <- read_format('data/gencode.v27.basic.annotation.gtf')
-
-
+gtf_df <- as.data.frame(gtf)
+all_genes <- unique(gtf_df$gene_name)
+all_genes_plus <- filter(gtf_df, strand=='+')[,'gene_name']%>%unique
+getOption("mc.cores", 2L)
 make_matrix <- function(gtf,gene,target_peaks){
+    #a=Sys.time()
     gtf_gene <- gtf[elementMetadata(gtf)[,'gene_name']==gene]
     gtf_gene_introns <- construct_introns(gtf_gene, update=TRUE)%>%as.data.frame
     
@@ -32,7 +37,8 @@ make_matrix <- function(gtf,gene,target_peaks){
     ex_int_bed <- filter(gtf_target_tx, feature%in%c('exon','intron'))%>%select( seqnames, start, end, feature,score,strand)
     #now lets make the distance bins
     # struct-1000000 -100000 -10000 -1000 TSS]  [TES +1000 +10000 +100000 +1000000
-    
+    #b=Sys.time()
+    #c=Sys.time()
     make_bins <- function(TSS, TES, locs, strand,chr){
         last=0
         upres=list()
@@ -58,7 +64,20 @@ make_matrix <- function(gtf,gene,target_peaks){
     
     all_features_range=GRanges(seqnames=all_features_bed$seqnames, ranges=IRanges(start=all_features_bed$start,end=all_features_bed$end),      
                                strand = all_features_bed$strand, feature=all_features_bed$feature)
-    peaks_range <- GRanges(seqnames=target_peaks$p_chr, ranges = IRanges(start=target_peaks$p_start,end = target_peaks$p_end),peaks=target_peaks$peak)    
+    
+    target_peaks_chr <- filter(target_peaks,p_chr%in%chr )
+    if(nrow(target_peaks_chr)==0){
+        print(paste0('chrom for ',gene,' not found in peak file'))
+        loc_names <- c(paste0('ds',locs/1000,'k'),paste0('us',locs/1000,'k'),'exon','intron','expression')
+        res <- matrix(data=0, nrow = 1, ncol = length(loc_names))
+        rownames(res) <- gene
+        colnames(res) <- loc_names
+        return(res)
+        
+    }
+    #d=Sys.time()
+    #e=Sys.time()
+    peaks_range <- GRanges(seqnames=target_peaks_chr$p_chr, ranges = IRanges(start=target_peaks_chr$p_start,end = target_peaks_chr$p_end),peaks=target_peaks_chr$peak)    
     hits <- findOverlaps(all_features_range,peaks_range)
     overlaps <- pintersect(all_features_range[queryHits(hits)], peaks_range[subjectHits(hits)])
     percentOverlap <- width(overlaps) / width( peaks_range[subjectHits(hits)])
@@ -81,12 +100,16 @@ make_matrix <- function(gtf,gene,target_peaks){
     colnames(res) <- loc_names
     res[,colnames(join_frame)] <- join_frame[1,]%>%as.numeric 
     rownames(res) <- gene
+    #f=Sys.time()
     return(res)
 }
-make_matrix(gtf,gene,target_peaks)
-a=Sys.time()
-all_plus_genes <- lapply(all_genes,function(x) make_matrix(gtf,x,target_peaks))
-b=Sys.time()
+
+
+k=Sys.time()
+all_plus_genes <- mclapply(all_genes,function(x) make_matrix(gtf,x,target_peaks),mc.cores = args[2])
+m=Sys.time()
+m-k
+
 #current runtime for 
 
 
