3dd3ae383752422c6a4dc17ff1783ca33138cc14
Vinay Swamy <swamyvinny@gmail.com>
2018-12-14 11:36:08 -0500

    added count file as arg and loads required modules

--- matrix_maker.sh
+++ matrix_maker.sh
@@ -5,8 +5,12 @@ gtf=$1
 peaks=$2
 prefix=$3
 output=$4
+counts=$5
 
 cd "$(dirname "$0")"
 
+module load bedtools
+module load R
+
 bash scripts/overlap_calculations.sh $gtf $peaks $prefix
-Rscript scripts/overlap_calculations_to_matrix.R $prefix $output 
+Rscript scripts/overlap_calculations_to_matrix.R $prefix $output $counts
