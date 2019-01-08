14a05cee1c9bf6154995b55e779cdd99b2b168a4
David McGaughey <davemcg@users.noreply.github.com>
2019-01-08 16:22:39 -0500

    make the prefix here, to remove need for user to provide

--- matrix_maker.sh
+++ matrix_maker.sh
@@ -3,14 +3,15 @@ set -euo pipefail
 
 gtf=$1
 peaks=$2
-prefix=$3
-output=$4
-counts=$5
+output=$3
+counts=$4
 
 cd "$(dirname "$0")"
 
 module load bedtools
 module load R
+
+prefix=$1_$2
 
 bash scripts/overlap_calculations.sh $gtf $peaks $prefix
 Rscript scripts/overlap_calculations_to_matrix.R $prefix $output $counts
